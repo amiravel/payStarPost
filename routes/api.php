25086232308e075071bac3ba5c611a84b062f64b
laravelgithubacademyit <laravelgithubacademyit@gmail.com>
2021-06-04 12:40:02 +0430

    extra controller been deleted.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterationController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
